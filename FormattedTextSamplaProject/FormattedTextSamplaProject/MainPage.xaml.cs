54c878fcbe9fe73ba6aae7c714a3318c4bce4b6e
David Eisler <eisler.david1@gmail.com>
2019-03-22 15:58:48 +0100

    initial commit

---
+++ FormattedTextSamplaProject/FormattedTextSamplaProject/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FormattedTextSamplaProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            var fs = new FormattedString();
+            fs.Spans.Add(new Span { Text = "All Androids: ", FontAttributes = FontAttributes.Bold });
+            fs.Spans.Add(new Span { Text = "When I add some new lines, it will stuck on all Androids." + Environment.NewLine + "Do sou see?" });
+            this.CrashingLabel.FormattedText = fs;
+        }
+    }
+}
